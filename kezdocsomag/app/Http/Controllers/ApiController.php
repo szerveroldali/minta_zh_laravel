2be5775e9ec1403d82a4a4a717a8baf54d159b7c
Tűri Erik <54734700+turierik@users.noreply.github.com>
2025-05-18 16:30:03 +0200

    Minta ZH

---
+++ kezdocsomag/app/Http/Controllers/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Location;
+use App\Models\Weather;
+use App\Models\Warning;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+
+}
